ac4e15f934e516c232413a47497e6aa2adf9a575
mdekhil <dekhil.medali@gmail.com>
2019-10-01 12:10:54 +0200

    show help when no arguments

--- Script/script.py
+++ Script/script.py
@@ -1,21 +1,32 @@
 import argparse
 import sys
 import os
-from pathlib import Path
+"""from pathlib import Path"""
 
 parser = argparse.ArgumentParser(description='Update Copyright for GAMS files.')
 parser.add_argument('-p', help='folder path for the repository')
 """parser.add_argument('-y', help='Current year') will be implemented later either with a date module or manually"""
 
+if len(sys.argv)==1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
+
+
 """data_folder = Path()"""
 args = parser.parse_args()
+print(args)
+print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+print(sys.argv)
+"""parser.print_help()"""
+
 """print(sys.argv[2])"""
 
 """argument check if sys.argv[1] """
 
 
 """ get the files list """
-path = str(sys.argv[2])
+"""path = str(sys.argv[2])
 
 files = []
 # r=root, d=directories, f = files
@@ -23,16 +34,12 @@ for r, d, f in os.walk(path):
     for file in f:
         if '.cpp' in file:
             files.append(os.path.join(r, file))
-"""        elif '.h' in file:
+        elif '.h' in file:
             files.append(os.path.join(r, file))
         elif '.pro' in file:
             files.append(os.path.join(r, file))
         elif '.pri' in file:
-            files.append(os.path.join(r, file))"""
+            files.append(os.path.join(r, file))
 for f in files:
-    lines = f.readlines()
-    
-    """text = open(f, "w+")
-    lines = list(f)"""
-    print(text)
+    print(f)"""
 
