16d564581cbb90c84fd9221945fddc1182b36ec0
mdekhil <dekhil.medali@gmail.com>
2019-10-09 11:12:55 +0200

    testing another read/write method

--- Script/script.py
+++ Script/script.py
@@ -2,15 +2,18 @@ import argparse
 import sys
 import os
 import csv
-
+import re
+'''
+R=re.compile("\d\d\d\d")
 def yearUpdate( filepath ):
     with open(filepath) as f:
         lines = [line.rstrip('\n') for line in open(filepath)]
-        print ("Read String is : ",lines[:5])
+        lines = [next(i) for x in range(5)]
+        for x in lines:
+            date = re.match("(\d\d\d\d)",str(lines(x))
         
-
+'''
 parser = argparse.ArgumentParser(description='Update Copyright for GAMS files.')
-
 parser.add_argument('-p', help='folder path for the repository')
 """parser.add_argument('-y', help='Current year') will be implemented later either with a date module or manually"""
 
@@ -49,7 +52,13 @@ for r, d, f in os.walk(path):
         elif file.endswith('.cpp'):
             files.append(os.path.join(r, file))
 for f in files:
-    yearUpdate(f)
+    '''yearUpdate(f)'''
+     with open(f) as f:
+    lines = [line.rstrip('\n') for line in open(f)]
+    '''lines = [next(i) for x in range(5)]'''
+    for x in lines:
+            date = re.match("(\d\d\d\d)",str(lines(x))
+            print(date)
 
 check = input('These files will be altered, are you sure you want to proceed? Y/N: ')
 if check == 'n' or check == 'N':
