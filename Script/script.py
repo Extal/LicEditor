6b79ee0524d857ffe56ff2abe5f7cbb9912431ea
mdekhil <dekhil.medali@gmail.com>
2019-10-22 16:18:54 +0200

    added exception argument and store the input in table

--- Script/script.py
+++ Script/script.py
@@ -33,6 +33,7 @@ def update(filepath, content):
 parser = argparse.ArgumentParser(description='Update Copyright for GAMS files.')
 parser.add_argument('--dry_run', help='show the user the files to be altered', action='store_true')
 parser.add_argument('-p', help='folder path for the repository')
+parser.add_argument('-e','--execption', nargs='+', help='Folders to be ignored')
 
 """if there are no arguments show help"""
 if len(sys.argv)==1:
@@ -54,6 +55,19 @@ dryrunmode = False
 if '--dry_run' in sys.argv:
     print('You are in a dry run mode. These are the files to be changed after executing the script:')
     dryrunmode = True
+print(sys.argv)
+exceptions = []
+if '--exception' in sys.argv or '-e' in sys.argv:
+    foldersIndex = sys.argv.index('-e')+1
+    if len(sys.argv) == foldersIndex:
+        print('you forgot to add exception folders')
+        exit()
+    exceptiontest = str(sys.argv[foldersIndex])
+    if exceptiontest[0] == '-':
+        print('you forgot to add exception folders')
+        exit()
+    exceptions = sys.argv[foldersIndex].split(",")
+print(exceptions)
 
 files = []
 # r=root, d=directories, f = files
